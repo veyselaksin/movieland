576ac270c87d4f4fcc7c68158a041ddce66fa9dd
Veysel Aksin <veyselaksn@gmail.com>
2023-03-24 07:14:16 +0300

    Movieland project created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react';
 
-function App() {
+const API_URL = 'https://www.omdbapi.com/?apikey=6bbf6deb';
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`).then((response) => response.json()).then((data) => console.log(data));
+  };
+  useEffect(() => {
+    searchMovies('batman');
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello World</h1>
   );
 }
 
